18d8d94c7434e376722c981cd57d7322e4af7e58
KunnuSherry <91347048+KunnuSherry@users.noreply.github.com>
2024-03-12 19:34:19 +0530

    This program creates factorial of a number by two methods.
    The Two methods are:
    1. for loop
    2. reduce method

---
+++ FactorialByTwoMethods.js
@@ -0,0 +1,17 @@
+// /* Factorial using for loop */
+// let a = prompt("enter the number");
+// let fac = 1;
+// for (let i = 1; i <= a ; i++) {
+//     fac *= i;
+// }
+// console.log(fac)
+
+/* using reduce */
+// let a = prompt("enter the number");
+// function Factorial(number){
+//     if(number<0){
+//         console.log("error");
+//     }
+//     const fac = Array.from({length : number}, (_,index) => index + 1).reduce((accumulator, currentvalue) => accumulator*currentvalue,1);
+//     console.log(fac);
+// }
